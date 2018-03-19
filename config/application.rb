4dc1dea317fb5c989dd1f05be22de5866f5b385e
koyanagi <koyakeiaaaaa@gmail.com>
2018-03-19 13:43:28 +0900

    change scaffold settings

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ Bundler.require(*Rails.groups)
 module RailsWebApp
   class Application < Rails::Application
     config.read_encrypted_secrets = true
+    config.generators do |g|
+      g.api true
+      g.test_framework :rspec
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
