50198b315d115bf5651762cbad8884105013395d
koyanagi <koyakeiaaaaa@gmail.com>
2018-03-25 15:28:37 +0900

    secure for google auth

--- app/controllers/viron_authtype_controller.rb
+++ app/controllers/viron_authtype_controller.rb
@@ -3,7 +3,7 @@ class VironAuthtypeController < ApplicationController
     render json: [{
                     type: 'oauth', # OAuth認証を利用する場合のtype
     provider: 'google', # OAuthを提供するプロバイダ。
-    url: 'https://makxastest.auth.ap-northeast-1.amazoncognito.com/login?response_type=code&client_id=196fbvmncvl8crgf2bf0lbu8hf&redirect_uri=https://localhost:8080/#/SkzXmh7qG/quick_view',
+    url: 'https://makxastest.auth.ap-northeast-1.amazoncognito.com/login?response_type=code&client_id=196fbvmncvl8crgf2bf0lbu8hf',
       method: 'POST',
     },{
                     type: 'signout',
