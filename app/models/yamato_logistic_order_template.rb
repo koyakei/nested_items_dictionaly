b3e600a4eb8c9e90b85321cfdc3f4c35dfed61ad
koyanagi <koyakeiaaaaa@gmail.com>
2018-03-27 16:57:39 +0900

    get yamato logistic order template from item id

--- app/models/yamato_logistic_order_template.rb
+++ app/models/yamato_logistic_order_template.rb
@@ -4,4 +4,41 @@ class YamatoLogisticOrderTemplate < ApplicationRecord
   belongs_to :yamato_handling_type_code
   has_one :logistic_order_template, as: :logistic_order_templatable
   belongs_to :creator, class_name: User.to_s
+
+  def set_values(id)
+    ActiveRecord::Base.connection.select_one(<<-SQL,
+      WITH RECURSIVE rec(id, parent_item_id,
+ depth, start_id
+  )AS (
+        SELECT
+         t1.id,
+         t1.parent_item_id,
+         1 ,
+         t1.id,
+         y1.yamato_size_item_code_id,
+         y1.yamato_packing_item_code_id,
+                 y1.yamato_handling_type_code_id
+        FROM (items t1 left join logistic_order_templates on (t1.id = logistic_order_templates.item_id)) 
+      left join yamato_logistic_order_templates  y1 on (y1.id = logistic_order_templates.logistic_order_templatable_id )
+        WHERE t1.id = #{id}
+        UNION ALL
+        SELECT
+        t1.id,
+        t1.parent_item_id,
+         rec.depth + 1,
+         rec.start_id,
+         COALESCE( rec.yamato_size_item_code_id, y1.yamato_size_item_code_id),
+         COALESCE( rec.yamato_packing_item_code_id, y1.yamato_packing_item_code_id),
+         COALESCE( rec.yamato_handling_type_code_id, y1.yamato_handling_type_code_id)
+        FROM (items t1 left join logistic_order_templates on (t1.id = logistic_order_templates.item_id)) 
+      left join yamato_logistic_order_templates  y1 on (y1.id = logistic_order_templates.logistic_order_templatable_id ), rec
+        WHERE  rec.parent_item_id =  t1.id )
+      SELECT
+        rec.id,depth, yamato_size_item_code_id, yamato_packing_item_code_id,
+        yamato_handling_type_code_id, ic.code_from , ic.code_to , ic.name_from , ic.name_to, ic.size, pc1.*
+      FROM (rec left join yamato_size_item_codes ic on( yamato_size_item_code_id = ic.id)) left join yamato_packing_item_codes pc1 on (yamato_packing_item_code_id = pc1.id)
+      order by depth desc limit 1
+                                             SQL
+    )
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,118 +1,118 @@
-# User.new(id: 1).save
-# maker = Maker.new(id: 1, description: "unknown")
-# maker.save
-# maker_alias = MakerAlias.new(name: "不明")
-# maker_alias.maker = Maker.first
-# maker_alias.creator = User.first
-# maker_alias.save!
-# maker = Maker.first
-# maker.maker_alias = maker_alias
-# maker.save
+User.new(id: 1).save
+maker = Maker.new(id: 1, description: "unknown")
+maker.save
+maker_alias = MakerAlias.new(name: "不明")
+maker_alias.maker = Maker.first
+maker_alias.creator = User.first
+maker_alias.save!
+maker = Maker.first
+maker.maker_alias = maker_alias
+maker.save
 
-# maker = Maker.new(id: 2, description: "Apple")
-# maker.save
-# maker_alias = MakerAlias.new(name: "Apple")
-# maker_alias.maker = Maker.first
-# maker_alias.creator = User.first
-# maker_alias.save!
-# maker.maker_alias = maker_alias
-# maker.save
-
-
+maker = Maker.new(id: 2, description: "Apple")
+maker.save
+maker_alias = MakerAlias.new(name: "Apple")
+maker_alias.maker = Maker.first
+maker_alias.creator = User.first
+maker_alias.save!
+maker.maker_alias = maker_alias
+maker.save
 
 
-# item_alias = ItemAlias.new
-# item_alias.name = "ALL"
-# item_alias.creator = User.first
-# item_alias.save!
 
-# item = Item.new(id: 1, maker_id: Maker.first.id,description: "全部")
-# item.creator = User.first
-# item.item_alias = ItemAlias.first
-# item.max_threshold_price = 2_147_483_647
-# item.min_threshold_price = 0
-# item.save!(validate: false)
-#
 
-# item_alias.item_id = item.id
-# item_alias.save!
-# CSV.foreach("db/seeds/csv/yamato_packing_item_codes.csv") do |row|
-#   YamatoPackingItemCode.new(code: row[0], name: row[1]).save
-# end
-#
-# CSV.foreach("db/seeds/csv/yamato_size_item_codes.csv") do |row|
-#   YamatoSizeItemCode.new(code_from: row[0], name_from: row[1],
-#                          code_to: row[2], name_to: row[3], size: row[4]).save
-# end
-#
-# CSV.foreach("db/seeds/csv/yamato_handling_type_codes.csv") do |row|
-#   YamatoHandlingTypeCode.new(code: row[0], name: row[1]).save
-# end
-#
-# yamato_logistic_order_template = YamatoLogisticOrderTemplate.new
-# yamato_logistic_order_template.yamato_size_item_code = YamatoSizeItemCode.first
-# yamato_logistic_order_template.yamato_packing_item_code = YamatoPackingItemCode.first
-# yamato_logistic_order_template.yamato_handling_type_code = YamatoHandlingTypeCode.first
-# yamato_logistic_order_template.creator = User.first
-# yamato_logistic_order_template.save!
-#
-# logistic_order_template = LogisticOrderTemplate.new
-# logistic_order_template.item = Item.first
-# logistic_order_template.logistic_order_templatable = YamatoLogisticOrderTemplate.first
-# logistic_order_template.creator = User.first
-# logistic_order_template.save!
-#
-#
-# item_alias = ItemAlias.new
-# item_alias.name = "mobile"
-# item_alias.creator = User.first
-# item_alias.save!
-#
-# item = Item.new(id: 2, maker_id: Maker.first.id, parent_item_id: 1)
-# item.creator = User.first
-# item.item_alias = item_alias
-# item.save!(validate: false)
-#
-# yamato_logistic_order_template = YamatoLogisticOrderTemplate.new
-# yamato_logistic_order_template.yamato_size_item_code = YamatoSizeItemCode.second
-# yamato_logistic_order_template.yamato_packing_item_code = YamatoPackingItemCode.first
-# yamato_logistic_order_template.yamato_handling_type_code = YamatoHandlingTypeCode.first
-# yamato_logistic_order_template.creator = User.first
-# yamato_logistic_order_template.save
-#
-# logistic_order_template = LogisticOrderTemplate.new
-# logistic_order_template.item = item
-# logistic_order_template.logistic_order_templatable = YamatoLogisticOrderTemplate.first
-# logistic_order_template.creator = User.first
-# logistic_order_template.save!
-#
-#
-# item_alias.item_id = item.id
-# item_alias = ItemAlias.new
-# item_alias.name = "iPhone"
-# item_alias.creator = User.first
-# item_alias.save!
-#
-# item = Item.new(id: 3, maker_id: 2, parent_item_id: 2)
-# item.creator = User.first
-# item.item_alias_id = item_alias.id
-# item.save!(validate: false)
-#
-#
-# item_alias.item_id = item.id
-# item_alias.save!
-#
-# item_alias = ItemAlias.new
-# item_alias.name = "iPhone 6"
-# item_alias.creator = User.first
-# item_alias.save!
-#
-# item = Item.new(id: 4, maker_id: Maker.first.id, parent_item_id: 3, has_child: false)
-# item.creator = User.first
-# item.item_alias_id = item_alias.id
-# item.save!(validate: false)
-# item_alias.item_id = item.id
-# item_alias.save!
+item_alias = ItemAlias.new
+item_alias.name = "ALL"
+item_alias.creator = User.first
+item_alias.save!
+
+item = Item.new(id: 1, maker_id: Maker.first.id,description: "全部")
+item.creator = User.first
+item.item_alias = ItemAlias.first
+item.max_threshold_price = 2_147_483_647
+item.min_threshold_price = 0
+item.save!(validate: false)
+
+
+item_alias.item_id = item.id
+item_alias.save!
+CSV.foreach("db/seeds/csv/yamato_packing_item_codes.csv") do |row|
+  YamatoPackingItemCode.new(code: row[0], name: row[1]).save
+end
+
+CSV.foreach("db/seeds/csv/yamato_size_item_codes.csv") do |row|
+  YamatoSizeItemCode.new(code_from: row[0], name_from: row[1],
+                         code_to: row[2], name_to: row[3], size: row[4]).save
+end
+
+CSV.foreach("db/seeds/csv/yamato_handling_type_codes.csv") do |row|
+  YamatoHandlingTypeCode.new(code: row[0], name: row[1]).save
+end
+
+yamato_logistic_order_template = YamatoLogisticOrderTemplate.new
+yamato_logistic_order_template.yamato_size_item_code = YamatoSizeItemCode.first
+yamato_logistic_order_template.yamato_packing_item_code = YamatoPackingItemCode.first
+yamato_logistic_order_template.yamato_handling_type_code = YamatoHandlingTypeCode.first
+yamato_logistic_order_template.creator = User.first
+yamato_logistic_order_template.save!
+
+logistic_order_template = LogisticOrderTemplate.new
+logistic_order_template.item = Item.first
+logistic_order_template.logistic_order_templatable = YamatoLogisticOrderTemplate.first
+logistic_order_template.creator = User.first
+logistic_order_template.save!
+
+
+item_alias = ItemAlias.new
+item_alias.name = "mobile"
+item_alias.creator = User.first
+item_alias.save!
+
+item = Item.new(id: 2, maker_id: Maker.first.id, parent_item_id: 1)
+item.creator = User.first
+item.item_alias = item_alias
+item.save!(validate: false)
+
+yamato_logistic_order_template = YamatoLogisticOrderTemplate.new
+yamato_logistic_order_template.yamato_size_item_code = YamatoSizeItemCode.second
+yamato_logistic_order_template.yamato_packing_item_code = YamatoPackingItemCode.first
+yamato_logistic_order_template.yamato_handling_type_code = YamatoHandlingTypeCode.first
+yamato_logistic_order_template.creator = User.first
+yamato_logistic_order_template.save
+
+logistic_order_template = LogisticOrderTemplate.new
+logistic_order_template.item = item
+logistic_order_template.logistic_order_templatable = YamatoLogisticOrderTemplate.first
+logistic_order_template.creator = User.first
+logistic_order_template.save!
+
+
+item_alias.item_id = item.id
+item_alias = ItemAlias.new
+item_alias.name = "iPhone"
+item_alias.creator = User.first
+item_alias.save!
+
+item = Item.new(id: 3, maker_id: 2, parent_item_id: 2)
+item.creator = User.first
+item.item_alias_id = item_alias.id
+item.save!(validate: false)
+
+
+item_alias.item_id = item.id
+item_alias.save!
+
+item_alias = ItemAlias.new
+item_alias.name = "iPhone 6"
+item_alias.creator = User.first
+item_alias.save!
+
+item = Item.new(id: 4, maker_id: Maker.first.id, parent_item_id: 3, has_child: false)
+item.creator = User.first
+item.item_alias_id = item_alias.id
+item.save!(validate: false)
+item_alias.item_id = item.id
+item_alias.save!
 
 
 item_alias = ItemAlias.new
