9dcc5785f4f54f9ecb984cc6d109a02a83ffcf90
Yusuke Saito <y.saito@mirai-souzou.jp>
2018-03-27 16:01:37 +0900

    yamato系再投入

--- app/models/yamato_packing_item_code.rb
+++ app/models/yamato_packing_item_code.rb
@@ -1,2 +1,3 @@
 class YamatoPackingItemCode < ApplicationRecord
+  has_many :yamato_logistic_order_templates
 end
--- app/models/yamato_size_item_code.rb
+++ app/models/yamato_size_item_code.rb
@@ -1,2 +1,3 @@
 class YamatoSizeItemCode < ApplicationRecord
+  has_many :yamato_logistic_order_templates
 end
