82de99cd27819948c35632b1d8af479846dc3651
koyanagi <koyakeiaaaaa@gmail.com>
2018-03-30 01:16:15 +0900

    fix association ( creator)

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -1,5 +1,5 @@
 class Accessory < ApplicationRecord
   belongs_to :base_item, class_name: Item.to_s
   belongs_to :accessory_item, class_name: Item.to_s
-  belongs_to :creator, class_name: User.to_s, foreign_key: :creator_id
+  belongs_to :creator, class_name: User.to_s
 end
