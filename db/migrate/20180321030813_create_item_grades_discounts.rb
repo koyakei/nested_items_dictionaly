454e1fb77920f06cf01a63f28cdada1cabb9c9db
koyanagi <koyakeiaaaaa@gmail.com>
2018-03-29 12:14:51 +0900

    全てのtable で creator を記録

--- db/migrate/20180321030813_create_item_grades_discounts.rb
+++ db/migrate/20180321030813_create_item_grades_discounts.rb
@@ -3,7 +3,7 @@ class CreateItemGradesDiscounts < ActiveRecord::Migration[5.1]
     create_table :item_grades_discounts do |t|
       t.references :item, foreign_key: true, null: false
       t.references :grade, foreign_key: true, null: false
-      t.references :user, foreign_key: true, null: false
+      t.references :creator, foreign_key: {to_table: :users}, null: false, default: 1
       t.float :discount_ration, limit: 53, null: false
 
       t.timestamps default: -> { 'NOW()' }
