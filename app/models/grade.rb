347fc66e961de2e74c90f3c03acfd1762321d6b7
koyanagi <koyakeiaaaaa@gmail.com>
2018-03-30 16:32:47 +0900

    add asso

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,3 +1,3 @@
 class Grade < ApplicationRecord
-  # has_many :item_grades_discounts
+  has_many :item_grades_discounts
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ApplicationRecord
-  # has_many :children_items, class_name: Item.to_s, foreign_key: :parent_item_id
-  # belongs_to :creator, class_name: User.to_s
-  # belongs_to :maker
-  # has_many :item_aliases
-  # belongs_to :parent_item, class_name: Item.to_s, optional: false
-  # has_many :costs_for_items
-  # has_many :logistic_order_templates
-  # has_many :accessories
-  # has_many :item_images
+  has_many :children_items, class_name: Item.to_s, foreign_key: :parent_item_id
+  belongs_to :creator, class_name: User.to_s
+  belongs_to :maker
+  has_many :item_aliases
+  belongs_to :parent_item, class_name: Item.to_s, optional: false
+  has_many :costs_for_items
+  has_many :logistic_order_templates
+  has_many :accessories
+  has_many :item_images
 
   def set_values(id)
     ActiveRecord::Base.connection.select_one(
