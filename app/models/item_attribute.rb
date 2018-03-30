65a848aee3ea5c32f8f838d0be1183dc8c4045b5
koyanagi <koyakeiaaaaa@gmail.com>
2018-03-30 14:49:07 +0900

    image attach 除去

--- app/models/item_attribute.rb
+++ app/models/item_attribute.rb
@@ -1,6 +1,6 @@
 class ItemAttribute < ApplicationRecord
-  belongs_to :item
-  # belongs_to :attribute
-  belongs_to :display_unit
-  belongs_to :creator, class_name: User.to_s
+  # belongs_to :item
+  # # belongs_to :attribute
+  # belongs_to :display_unit
+  # belongs_to :creator, class_name: User.to_s
 end
