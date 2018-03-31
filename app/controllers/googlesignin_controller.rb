4cc078f3a293269aa9bbbaf14a2a6e6f369028fe
koyanagi <koyakeiaaaaa@gmail.com>
2018-03-31 18:39:49 +0900

    google signin HTTP verb variation

--- app/controllers/googlesignin_controller.rb
+++ app/controllers/googlesignin_controller.rb
@@ -2,4 +2,16 @@ class GooglesigninController < ApplicationController
   def index
     render json: {}
   end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
