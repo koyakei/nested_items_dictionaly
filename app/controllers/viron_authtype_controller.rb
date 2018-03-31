4f220a4dbce47a09a827e8bf6d496d38a27647ab
koyanagi <koyakeiaaaaa@gmail.com>
2018-03-31 12:28:54 +0900

    add Viron auth type

--- app/controllers/viron_authtype_controller.rb
+++ app/controllers/viron_authtype_controller.rb
@@ -1,9 +1,9 @@
 class VironAuthtypeController < ApplicationController
   def index
-    render json: [{
+    render json: [{type: "email", provider: "viron-demo", url: "/signin", method: "POST"},{
                     type: 'oauth', # OAuth認証を利用する場合のtype
     provider: 'google', # OAuthを提供するプロバイダ。
-    url: 'https://makxastest.auth.ap-northeast-1.amazoncognito.com/login?response_type=code&client_id=196fbvmncvl8crgf2bf0lbu8hf',
+    url: '/googlesignin',
       method: 'POST',
     },{
                     type: 'signout',
