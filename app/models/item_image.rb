7ceb09bf6437de7f205bf83185708f04f1fb9e9a
koyanagi <koyakeiaaaaa@gmail.com>
2018-03-31 12:55:48 +0900

    enable active storage

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,5 +1,5 @@
 class ItemImage < ApplicationRecord
   belongs_to :item
-  # has_one_attached :image
+  has_one_attached :image
   belongs_to :creator, class_name: User.to_s
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_21_104452) do
+ActiveRecord::Schema.define(version: 2018_03_23_034113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,27 @@ ActiveRecord::Schema.define(version: 2018_03_21_104452) do
     t.index ["accessory_item_id"], name: "index_accessories_on_accessory_item_id"
     t.index ["base_item_id"], name: "index_accessories_on_base_item_id"
     t.index ["creator_id"], name: "index_accessories_on_creator_id"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "additional_conditions", force: :cascade do |t|
