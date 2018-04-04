0453637e3b793f98ccc2058b29f1b3e5ca8a2e57
koyanagi <koyakeiaaaaa@gmail.com>
2018-04-04 22:10:14 +0900

    add fabricators to yamato logistic template

--- spec/fabricators/logistic_order_templatable_yamato_logistic_order_template_fabricator.rb
+++ spec/fabricators/logistic_order_templatable_yamato_logistic_order_template_fabricator.rb
@@ -1,10 +1,7 @@
 Fabricator('LogisticOrderTemplatable::Yamato::LogisticOrderTemplate') do
-  id                            1
-  yamato_size_item_code_id      1
-  yamato_packing_item_code_id   1
-  yamato_handling_type_code1_id 1
-  yamato_handling_type_code2_id 1
-  creator_id                    1
-  created_at                    "2018-04-04 18:47:44"
-  updated_at                    "2018-04-04 18:47:44"
+  yamato_size_item_code      LogisticOrderTemplate::Yamato::LogisticSize.first!
+  yamato_packing_item_code   LogisticOrderTemplate::Yamato::PackingItemCode.first!
+  yamato_handling_type_code1 LogisticOrderTemplate::Yamato::HandlingTypeCode.first!
+  yamato_handling_type_code2 nil
+  creator                    User.first!
 end
