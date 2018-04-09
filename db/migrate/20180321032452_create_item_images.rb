5c0a26ce2c5efeef8f4f010c8ab8c52ddc210684
koyanagi <koyakeiaaaaa@gmail.com>
2018-04-09 18:26:19 +0900

    edit item_images url

--- db/migrate/20180321032452_create_item_images.rb
+++ db/migrate/20180321032452_create_item_images.rb
@@ -2,11 +2,11 @@ class CreateItemImages < ActiveRecord::Migration[5.1]
   def change
     create_table :item_images do |t|
       t.references :item, foreign_key: true, null: false
-      t.text :image_path, null: false
+      t.text :url
       t.text :name
       t.text :description
-      t.references :creator, foreign_key: {to_table: :users}, null: false, default: 0
-      t.timestamps default: -> { 'NOW()' }
+      t.references :creator, foreign_key: { to_table: :users }, null: false, default: 0
+      t.timestamps default: -> { "NOW()" }
     end
   end
 end
