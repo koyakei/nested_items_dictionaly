a29bbe39cdfbc6f857c405774dac922a70ea0fba
koyanagi <koyakeiaaaaa@gmail.com>
2018-04-09 15:23:39 +0900

    not use AWS ES in local

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,7 @@
-Searchkick.aws_credentials = {
-  access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-  secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-  region: "ap-northeast-1"
-}
+unless Rails.env.test? || Rails.env.development?
+  Searchkick.aws_credentials = {
+    access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: "ap-northeast-1"
+  }
+end
