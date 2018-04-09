53aeeee2eeaf731c7f27febdc9b13d8f799acae2
koyanagi <koyakeiaaaaa@gmail.com>
2018-04-09 16:29:14 +0900

    change unique constraint

--- db/migrate/20180320055426_create_display_units.rb
+++ db/migrate/20180320055426_create_display_units.rb
@@ -3,10 +3,10 @@ class CreateDisplayUnits < ActiveRecord::Migration[5.1]
     create_table :display_units do |t|
       t.references :standard_unit, foreign_key: true, null: false
       t.text :name, limit: 5, null: false
-      t.float :display_ratio, limit:53, null: false
-      t.references :creator, foreign_key: {to_table: :users}, null: false, default: 0
-      t.timestamps default: -> { 'NOW()' }
+      t.float :display_ratio, limit: 53, null: false
+      t.references :creator, foreign_key: { to_table: :users }, null: false, default: 0
+      t.timestamps default: -> { "NOW()" }
     end
-    add_index :display_units, :name, unique: true
+    add_index :display_units, [:name, :standard_unit_id], unique: true
   end
 end
