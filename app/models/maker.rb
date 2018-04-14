80d9b851741b0a28ada5efb18b57f67b2ba370ba
koyanagi <koyakeiaaaaa@gmail.com>
2018-04-14 21:57:14 +0900

    add association reindex

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -1,6 +1,14 @@
 class Maker < ApplicationRecord
   searchkick
-
+  has_many :items
   has_many :maker_aliases
   belongs_to :creator, class_name: User.to_s
+
+  after_commit :reindex_items
+
+  def reindex_items
+    items.map {
+      |item| item.reindex
+    }
+  end
 end
