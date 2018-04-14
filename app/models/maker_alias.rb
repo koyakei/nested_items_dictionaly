94c04cf40f3b09d2e29e4ffb9bb165af607a39b0
koyanagi <koyakeiaaaaa@gmail.com>
2018-04-14 22:10:33 +0900

    add association searchkick update in maker_alias

--- app/models/maker_alias.rb
+++ app/models/maker_alias.rb
@@ -1,4 +1,10 @@
 class MakerAlias < ApplicationRecord
+  searchkick
   belongs_to :maker
   belongs_to :creator, class_name: User.to_s, foreign_key: :creator_id
+
+  after_commit :reindex_maker
+  def reindex_maker
+    maker.reindex
+  end
 end
