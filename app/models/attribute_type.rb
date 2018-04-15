9e682b3b37bccd66ca4ea5eb25d4a6a638f416db
koyanagi <koyakeiaaaaa@gmail.com>
2018-04-15 12:30:44 +0900

    fix relations

--- app/models/attribute_type.rb
+++ app/models/attribute_type.rb
@@ -2,4 +2,5 @@ class AttributeType < ApplicationRecord
   belongs_to :standard_unit
   has_many :item_attribute_types
   belongs_to :creator, class_name: User.to_s
+  has_many :items, through: :item_attribute_types
 end
