451dd6f919980ce7e777fd9be45acf1e27b28f6c
koyanagi <koyakeiaaaaa@gmail.com>
2018-04-15 09:06:44 +0900

    reindex item from maker alias

--- app/models/maker_alias.rb
+++ app/models/maker_alias.rb
@@ -2,9 +2,13 @@ class MakerAlias < ApplicationRecord
   searchkick
   belongs_to :maker
   belongs_to :creator, class_name: User.to_s, foreign_key: :creator_id
+  has_many :items, through: :maker
 
   after_commit :reindex_maker
   def reindex_maker
     maker.reindex
+    items.map {
+      |item| item.reindex
+    }
   end
 end
