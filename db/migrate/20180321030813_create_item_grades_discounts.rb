3955c0f57c800d1dc85b024681b1b4ab1937664d
koyanagi <koyakeiaaaaa@gmail.com>
2018-04-18 16:37:33 +0900

    change schema add int discount_price add default value discount_*

--- db/migrate/20180321030813_create_item_grades_discounts.rb
+++ db/migrate/20180321030813_create_item_grades_discounts.rb
@@ -4,9 +4,8 @@ class CreateItemGradesDiscounts < ActiveRecord::Migration[5.1]
       t.references :item, foreign_key: { on_update: :cascade }, null: false
       t.references :grade, foreign_key: { on_update: :cascade }, null: false
       t.references :creator, foreign_key: { to_table: :users, on_update: :cascade }, null: false, default: 0
-      t.float :discount_ration, limit: 53, null: false
-
-      t.timestamps default: -> { 'NOW()' }
+      t.float :discount_ratio, limit: 53, null: false, default: 0
+      t.timestamps default: -> { "NOW()" }
     end
   end
 end
--- db/migrate/20180321104450_create_item_additional_conditions.rb
+++ db/migrate/20180321104450_create_item_additional_conditions.rb
@@ -3,7 +3,8 @@ class CreateItemAdditionalConditions < ActiveRecord::Migration[5.1]
     create_table :item_additional_conditions do |t|
       t.references :item, foreign_key: true
       t.references :additional_condition, foreign_key: true
-      t.float :discount_ratio, limit: 53
+      t.float :discount_ratio, limit: 53, null: false, default: 0
+      t.integer :discount_price, null: false, default: 0
       t.references :creator, foreign_key: { to_table: :users, on_update: :cascade }, null: false, default: 0
       t.timestamps
     end
