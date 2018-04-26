257ef7d56f75409e6955b23924dc06c00415d3a3
koyanagi <koyakeiaaaaa@gmail.com>
2018-04-26 19:00:55 +0900

    add basic auth for admin view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
+  unless Rails.env.development?
+    http_basic_authenticate_with name: "product", password: ENV["BASIC_PASSWORD"]
+  end
 end
