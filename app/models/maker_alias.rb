59c1218968981258a4bdfb4d47dd7b689768b58d
koyakei <koyakeiaaaaa@gmail.com>
2018-11-19 09:46:58 +0900

    fix syntax

--- app/models/maker_alias.rb
+++ app/models/maker_alias.rb
@@ -1,4 +1,4 @@
-class MakerAlias < ApplicationRecord=
+class MakerAlias < ApplicationRecord
   belongs_to :maker
   belongs_to :creator, class_name: User.to_s, foreign_key: :creator_id
   has_many :items, through: :maker
