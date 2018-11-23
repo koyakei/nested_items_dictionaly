d0547791f6bf21f5a4cc6523e260527a2f8c914a
koyakei <koyakeiaaaaa@gmail.com>
2018-11-23 15:55:56 +0900

    update credential for searchkick

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,8 @@
+Searchkick.redis = Redis.new
 unless Rails.env.test? || Rails.env.development?
   Searchkick.aws_credentials = {
-    access_key_id: Rails.application.credentials.aws.access_key_id,
-    secret_access_key: Rails.application.credentials.aws.secret_access_key,
+    access_key_id: Rails.application.credentials[Rails.env.to_sym][:aws][:access_key_id],
+    secret_access_key: Rails.application.credentials[Rails.env.to_sym][:aws][:secret_access_key],
     region: "ap-northeast-1"
   }
 end
